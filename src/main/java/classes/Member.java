7bed4a7e1ba63fe2e13eb7b12f0b53d1a351eb26
anis <melaimiani@cy-tech.fr>
2024-05-09 18:15:58 +0200

    creation de Member

---
+++ src/main/java/classes/Member.java
@@ -0,0 +1,23 @@
+package classes;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Member {
+    private String lastName;
+    private String name;
+    private Date birthDate;
+    private String mail;
+    private List<Document> borrowedBooks;
+
+    public Member(String lastName, String name, Date birthDate, String mail){
+        this.borrowedBooks = new ArrayList<Document>();
+        this.lastName = lastName;
+        this.name = name;
+        this.birthDate = birthDate;
+        this.mail = mail;
+    }
+    public register(Me)
+
+}
