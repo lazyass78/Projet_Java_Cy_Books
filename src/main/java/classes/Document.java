61e5cada30a068a9224d601983a30a753bce2af9
lazyass78 <lazizi.yassine78@gmail.com>
2024-05-10 12:30:20 +0200

    mise de la classe document en abstract, et retirage du constructeur et get/set

--- src/main/java/classes/Document.java
+++ src/main/java/classes/Document.java
@@ -4,9 +4,9 @@ package classes;
 import java.util.List;
 
 /**
- * a document from the library
+ * a document from the library, it's an abstract class because we are going to make more precise document, like a novel ...
  */
-public class Document {
+public abstract class Document {
     public String name;
     public String author;
     public int year;
@@ -14,40 +14,4 @@ public class Document {
     public String ISBN;
     public int stock;
     public List<Sujet> topics;
-
-    public Document(String name,String author,int year,String editor,String ISBN,int stock,List<Sujet> topics){
-        this.name=name;
-        this.author=author;
-        this.year=year;
-        this.editor=editor;
-        this.ISBN=ISBN;
-        this.topics=topics;
-
-    }
-    public String getName() {
-        return name;
-    }
-    public String getAuthor() {
-        return author;
-    }
-    public int getYear() {
-        return year;
-    }
-    public String getEditor() {
-        return editor;
-    }
-    public String getISBN() {
-        return ISBN;
-    }
-    public List<Sujet> getTopics() {
-        return topics;
-    }
-
-    public int getStock() {
-        return stock;
-    }
-
-    public void setStock(int stock) {
-        this.stock = stock;
-    }
 }
