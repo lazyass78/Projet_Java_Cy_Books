e177c9d3db892355eed7e0f33f8b4c1d487e119c
lazyass78 <lazizi.yassine78@gmail.com>
2024-05-11 13:13:03 +0200

    class borrow

---
+++ src/main/java/classes/Borrowing.java
@@ -0,0 +1,31 @@
+package classes;
+
+import java.util.Date;
+
+public class Borrowing {
+    private Document document;
+
+    private Member member;
+
+    private Date borrowingDate;
+
+    private Date returnDate;
+
+    public Borrowing(Document document, Member member, Date borrowingDate, Date returnDate){
+        this.document = document;
+        this.member = member;
+        this.borrowingDate = borrowingDate;
+        this.returnDate = returnDate;
+    }
+
+    public boolean extendDate(Date extendDate) {
+        // vérifier si la date donné est plus loin que celle qu'on a
+        if(extendDate.after(this.returnDate)) {
+            this.returnDate = extendDate;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
