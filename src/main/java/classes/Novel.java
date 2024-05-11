ed1d77f0e7973237d437bd444879c3c50e3c6733
anis <melaimiani@cy-tech.fr>
2024-05-11 16:31:46 +0200

    class novel et poetry

--- src/main/java/classes/Novel.java
+++ src/main/java/classes/Novel.java
@@ -1,4 +1,9 @@
-package classes;
+package classes,
+
+import java.util.List,
 
 public class Novel extends Document{
+    public Novel(String name, String author, int year, String editor, String ISBN, int stock, List<Topic> topics) {
+        super(name, author, year, editor, ISBN, stock, topics);
+    }
 }
--- src/main/java/classes/Poetry.java
+++ src/main/java/classes/Poetry.java
@@ -1,4 +1,9 @@
 package classes;
 
+import java.util.List;
+
 public class Poetry extends Document{
+    public Poetry(String name, String author, int year, String editor, String ISBN, int stock, List<Topic> topics) {
+        super(name, author, year, editor, ISBN, stock, topics);
+    }
 }
