afbfeea8ed87ea4852730bc88a2a77691191aa3e
Rithan04 <julirithan29@gmail.com>
2024-05-11 16:33:12 +0200

    creation du package + class doc et sujet + membres private +classes filles

--- src/main/java/classes/Novel.java
+++ src/main/java/classes/Novel.java
@@ -1,6 +1,6 @@
-package classes,
+package classes;
 
-import java.util.List,
+import java.util.List;
 
 public class Novel extends Document{
     public Novel(String name, String author, int year, String editor, String ISBN, int stock, List<Topic> topics) {
