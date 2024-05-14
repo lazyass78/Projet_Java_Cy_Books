fe49f46f36f309c82b5051c96635a66c890c0d68
cytech <betouz92@gmail.com>
2024-05-14 16:51:41 +0200

    Fenetre

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,12 @@
 module Controller {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.xml;
 
     opens Controller to javafx.fxml;
 
     exports Controller;
+    exports Test;
+
 }
+
