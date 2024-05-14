fd4e40bee76a2c740df6582ad78682cde2de4938
lazyass78 <lazizi.yassine78@gmail.com>
2024-05-14 12:01:45 +0200

    Button fait mais pas encore fonctionnel

---
+++ src/main/java/Controller/CYBooksHomeController.java
@@ -0,0 +1,23 @@
+package Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class CYBooksHomeController {
+
+    @FXML private Button Emprunts;
+    @FXML private Button Adhérents;
+    @FXML private Button Livres;
+
+    public void Page_borrowing() {
+
+    }
+
+    public void Page_member(){
+
+    }
+
+    public void Page_document(){
+
+    }
+}
