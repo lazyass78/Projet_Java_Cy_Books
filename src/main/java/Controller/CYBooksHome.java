67cdf82a170ff2a6d3f18a7e004d3deb4f78efb1
lazyass78 <lazizi.yassine78@gmail.com>
2024-05-16 13:13:33 +0200

    commentary on CYBooksHome

--- src/main/java/Controller/CYBooksHome.java
+++ src/main/java/Controller/CYBooksHome.java
@@ -9,18 +9,30 @@ import java.io.IOException;
 
 public class CYBooksHome extends Application {
 
+    /**
+     * This method is called when the JavaFX application is launched.
+     * It loads the "CYBooks_Home.fxml" file using FXMLLoader,
+     * creates a new Scene with the loaded content, and displays it on the stage.
+     * @param stage The primary stage of the JavaFX application.
+     * @throws IOException If an error occurs while loading the FXML file.
+     */
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(CYBooksHome.class.getResource("CYBooks_Home.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
 
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+        stage.setTitle("CYBooks"); // Sets the title of the stage
+        stage.setScene(scene);  // Sets the scene to be displayed on the stage
+        stage.show(); // Shows the stage
     }
 
+    /**
+     * The main entry point for the JavaFX application.
+     * It launches the JavaFX application by calling the `launch()` method.
+     * @param args The command-line arguments passed to the application.
+     */
     public static void main(String[] args) {
-        launch();
+        launch(); // Launches the JavaFX application
     }
 
 }
