73912799d3d22bd291591c1eafc8bfc4b56a44c2
lazyass78 <lazizi.yassine78@gmail.com>
2024-05-24 00:28:58 +0200

    redirection button cancel delete borrow

--- src/main/java/Controller/CYBooksDeleteBorrowing.java
+++ src/main/java/Controller/CYBooksDeleteBorrowing.java
@@ -1,4 +1,42 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
 public class CYBooksDeleteBorrowing {
+
+    @FXML private AnchorPane mainContainer;
+    public void DeleteBorrowing(ActionEvent actionEvent) {
+        loadView("CYBooks_Borrowing");
+    }
+
+    public void CancelDelete(ActionEvent actionEvent) {
+
+    }
+
+    @FXML
+    private void loadView(String fxmlFileName) {
+        try {
+            if (mainContainer == null) {
+                System.err.println("Erreur : mainContainer n'a pas été correctement initialisé.");
+                return;
+            }
+
+            // Charge le fichier FXML de la vue spécifiée
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(fxmlFileName));
+            Parent view = fxmlLoader.load();
+
+            // Remplace le contenu actuel du conteneur principal par le contenu de la nouvelle vue
+            mainContainer.getChildren().clear();
+            mainContainer.getChildren().add(view);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
