28706e5c0e05884d7c4865ed6b269cb300275dad
Rithan04 <julirithan29@gmail.com>
2024-05-23 15:00:19 +0200

    filtrage corrigé + classes record dans model

--- src/main/java/Model/Borrowing.java
+++ src/main/java/Model/Borrowing.java
@@ -1,102 +1,112 @@
 package Model;
 
-import java.util.Date;
+import java.time.LocalDate;
 
-/**
- * Association Class Borrowing
- * Represents a borrowing association between a document and a member.
- */
 public class Borrowing {
-    /** The document being borrowed. */
-    private Document document;
+    private String isbn;
+    private String memberMail;
+    private String title;
+    private String author;
+    private int year;
+    private String editor;
+    private int stock;
+    private String topics;
+    private LocalDate borrowingDate;
+    private LocalDate returnDate;
 
-    /** The member who borrowed the document. */
-    private Member member;
-
-    /** The date when the document was borrowed. */
-    private Date borrowingDate;
-
-    /** The date when the document is expected to be returned. */
-    private Date returnDate;
-    private boolean status;
-
-    /**
-     * Constructor
-     * Constructs a new Borrowing object with the specified document, member, borrowing date, and return date.
-     * @param document
-     * @param member
-     * @param borrowingDate
-     * @param returnDate
-     */
-    public Borrowing(Document document, Member member, Date borrowingDate, Date returnDate, Boolean status){
-        this.document = document;
-        this.member = member;
+        public Borrowing(String isbn, String memberMail, String title, String author, int year, String editor, int stock, String topics, LocalDate borrowingDate, LocalDate returnDate) {
+        this.isbn = isbn;
+        this.memberMail = memberMail;
+        this.title=title;
+        this.author = author;
+        this.year = year;
+        this.editor = editor;
+        this.stock = stock;
+        this.topics = topics;
         this.borrowingDate = borrowingDate;
-        this.returnDate = returnDate;
-        this.status = status;
+        this.returnDate=returnDate;
+
+    }
+    public String getIsbn() {
+        return isbn;
     }
 
-    public Date getBorrowingDate() {
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getMemberMail() {
+        return memberMail;
+    }
+
+    public void setMemberMail(String memberMail) {
+        this.memberMail = memberMail;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getEditor() {
+        return editor;
+    }
+
+    public void setEditor(String editor) {
+        this.editor = editor;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getTopics() {
+        return topics;
+    }
+
+    public void setTopics(String topics) {
+        this.topics = topics;
+    }
+
+    public LocalDate getBorrowingDate() {
         return borrowingDate;
     }
 
-    public Document getDocument() {
-        return document;
-    }
-
-    public Date getReturnDate() {
-        return returnDate;
-    }
-
-    public Member getMember() {
-        return member;
-    }
-
-    public void setBorrowingDate(Date borrowingDate) {
+    public void setBorrowingDate(LocalDate borrowingDate) {
         this.borrowingDate = borrowingDate;
     }
 
-    public void setDocument(Document document) {
-        this.document = document;
+    public LocalDate getReturnDate() {
+        return returnDate;
     }
 
-    public void setMember(Member member) {
-        this.member = member;
-    }
-
-    public void setReturnDate(Date returnDate) {
+    public void setReturnDate(LocalDate returnDate) {
         this.returnDate = returnDate;
     }
 
-    public void setStatus(boolean status) {
-        this.status = status;
-    }
 
-    /**
-     * Extends the return date of the borrowing.
-     *
-     * @param extendDate the new return date to be set
-     * @return true if the return date is successfully extended, false otherwise
-     */
-    public boolean extendDate(Date extendDate) {
-        if(extendDate.after(this.returnDate)) {
-            this.returnDate = extendDate;
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-
-    public boolean checkStatus() {
-        if (this.status) {
-            System.out.println("Livre emprunté");
-            return true;
-        }
-        else {
-            System.out.println("Livre disponible");
-            return false;
-        }
-
-    }
 }
+
---
+++ src/main/java/Model/BorrowingInutile.java
@@ -0,0 +1,102 @@
+package Model;
+
+import java.util.Date;
+
+/**
+ * Association Class Borrowing
+ * Represents a borrowing association between a document and a member.
+ */
+public class BorrowingInutile {
+    /** The document being borrowed. */
+    private Document document;
+
+    /** The member who borrowed the document. */
+    private MemberInutile member;
+
+    /** The date when the document was borrowed. */
+    private Date borrowingDate;
+
+    /** The date when the document is expected to be returned. */
+    private Date returnDate;
+    private boolean status;
+
+    /**
+     * Constructor
+     * Constructs a new Borrowing object with the specified document, member, borrowing date, and return date.
+     * @param document
+     * @param member
+     * @param borrowingDate
+     * @param returnDate
+     */
+    public BorrowingInutile(Document document, MemberInutile member, Date borrowingDate, Date returnDate, Boolean status){
+        this.document = document;
+        this.member = member;
+        this.borrowingDate = borrowingDate;
+        this.returnDate = returnDate;
+        this.status = status;
+    }
+
+    public Date getBorrowingDate() {
+        return borrowingDate;
+    }
+
+    public Document getDocument() {
+        return document;
+    }
+
+    public Date getReturnDate() {
+        return returnDate;
+    }
+
+    public MemberInutile getMember() {
+        return member;
+    }
+
+    public void setBorrowingDate(Date borrowingDate) {
+        this.borrowingDate = borrowingDate;
+    }
+
+    public void setDocument(Document document) {
+        this.document = document;
+    }
+
+    public void setMember(MemberInutile member) {
+        this.member = member;
+    }
+
+    public void setReturnDate(Date returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    /**
+     * Extends the return date of the borrowing.
+     *
+     * @param extendDate the new return date to be set
+     * @return true if the return date is successfully extended, false otherwise
+     */
+    public boolean extendDate(Date extendDate) {
+        if(extendDate.after(this.returnDate)) {
+            this.returnDate = extendDate;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public boolean checkStatus() {
+        if (this.status) {
+            System.out.println("Livre emprunté");
+            return true;
+        }
+        else {
+            System.out.println("Livre disponible");
+            return false;
+        }
+
+    }
+}
