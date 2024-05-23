dff8e87c0af8489dbf0fac745cb65b86c25266f3
anis <melaimiani@cytech.fr>
2024-05-23 17:41:23 +0200

    stat

--- src/main/java/Controller/CYBooksStatisticsController.java
+++ src/main/java/Controller/CYBooksStatisticsController.java
@@ -10,7 +10,24 @@ import javafx.scene.chart.XYChart;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.sql.*;
 
 public class CYBooksStatisticsController {
@@ -65,13 +82,52 @@ public class CYBooksStatisticsController {
         // Code to fetch book title from database based on ISBN
         // Replace this with your actual database query
         try {
-            String query = "SELECT title FROM books WHERE isbn = ?";
+            String query = "SELECT * FROM books WHERE isbn = ?";
             try (PreparedStatement stmt = connection.prepareStatement(query)) {
                 stmt.setString(1, isbn);
                 try (ResultSet rs = stmt.executeQuery()) {
                     if (rs.next()) {
-                        return rs.getString("title");
+                        String title = "";
+                        if (!isbn.isEmpty()) {
+                            String apiUrl = "https://gallica.bnf.fr/SRU?operation=searchRetrieve&version=1.2";
+                            String encodedQuery = URLEncoder.encode(isbn, "UTF-8");
+                            String searchQuery = "query=dc.identifier%20all%20" + encodedQuery;
+                            String url = apiUrl + "&" + searchQuery;
+                            System.out.println(url);
+
+                            HttpClient client = HttpClient.newHttpClient();
+                            HttpRequest request = HttpRequest.newBuilder()
+                                    .uri(new URI(url))
+                                    .build();
+
+                            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+                            String responseBody = response.body();
+
+                            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+                            DocumentBuilder builder = factory.newDocumentBuilder();
+                            Document doc = builder.parse(new InputSource(new StringReader(responseBody)));
+
+
+                            NodeList titleNodes = doc.getElementsByTagName("dc:title");
+                            if (titleNodes.getLength() > 0) {
+                                Element titleElement = (Element) titleNodes.item(0);
+                                title = titleElement.getTextContent();
+                                return title;
+                            }
+                        }
                     }
+                } catch (UnsupportedEncodingException e) {
+                    throw new RuntimeException(e);
+                } catch (URISyntaxException e) {
+                    throw new RuntimeException(e);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                } catch (ParserConfigurationException e) {
+                    throw new RuntimeException(e);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                } catch (SAXException e) {
+                    throw new RuntimeException(e);
                 }
             }
         } catch (SQLException e) {
